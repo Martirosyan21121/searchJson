52bb8d274e3f6f329b8e6a04a6259130f391a521
Narek Martirosyan <narekmartirosyanbm21@gmail.com>
2024-04-04 15:45:31 +0400

    search json

--- js/search.js
+++ js/search.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $('#search').keyup(function(){
+$(document).ready(function () {
+    $('#search').keyup(function () {
         $('#result').html('');
         let searchInput = $('#search').val();
         let ignoreCase = new RegExp(searchInput, "i");
@@ -7,38 +7,50 @@ $(document).ready(function(){
 
         let generateLetter = generateCharMap()
 
+        $.getJSON('json/data.json', function (data) {
+            $.each(data, function (key, value) {
 
-        $.getJSON('json/data.json', function(data) {
-            $.each(data, function(key, value){
+                let armenianSearchInput = convertToArmenian(searchInput.toLowerCase());
 
-                let armenianSearchInput = '';
-                for (let i = 0; i < searchInput.length; i++) {
-                    const englishLetter = searchInput[i].toLowerCase();
-                    armenianSearchInput += generateLetter[englishLetter] || searchInput[i];
-                }
-                if (value.name.search(ignoreCase, unicode) !== -1 || value.name.toLowerCase().includes(armenianSearchInput.toLowerCase()))
-                {
-                    console.log(value.name, 'name')
-                    console.log(value.id, 'id')
-                    console.log(key.id, 'id key')
-                    $('#result').append('<li class="list-group-item link-class">'+ ' ID -> '+ value.id + ' | Name ->      ' + value.name + '</li>');
+                console.log(armenianSearchInput, 'armenianSearchInput')
+                if (value.name.search(ignoreCase, unicode) !== -1 ||
+                    value.name.toLowerCase().includes(armenianSearchInput.toLowerCase())) {
+
+                    $('#result').append('<li class="list-group-item link-class">' + ' ID -> ' + value.id + ' | Name ->      ' + value.name + '</li>');
 
                 }
             });
         });
+        function convertToArmenian(input) {
+            let armenianString = ''
+            let i = 0;
+            while (i < input.length) {
+                console.log(i, 'i')
+                if (i < input.length - 1 && generateLetter[input.substr(i, 2)]) {
+                    armenianString += generateLetter[input.substr(i, 2)];
+                    i += 2;
+                } else {
+                        const englishLetter = input[i].toLowerCase();
+                        armenianString += generateLetter[englishLetter] || input[i];
+                        console.log(armenianString, 'Armenian letter')
+                        i++;
+                }
+            }
+            return armenianString;
+        }
     });
-});
 
-function generateCharMap() {
-    const englishLetters = 'abcdefghijklmnopqrstuvwxyzr';
-    const armenianLetters = 'աբցդեֆգհիջկլմնոպքռստւվւխյզր';
-    const charMap = {};
+    function generateCharMap() {
+        const englishLetters = ['a', 'b', 'c', 'd', 'e', 'f',
+            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'g', 'r', 's', 't', 'v', 'x', 'w', 'y', 'z', 'u', 'ch', 'sh', 'ev'];
+        const armenianLetters = ['ա', 'բ', 'ց', 'դ', 'ե', 'ֆ', 'գ', 'հ', 'ի', 'ջ',
+            'կ', 'լ', 'մ', 'ն', 'օ', 'պ', 'գ', ['ռ' | 'ր'], 'ս', 'տ', 'վ', 'խ', 'ո', 'յ', 'զ', 'ու', 'չ', 'շ', 'և'];
+        const charMap = {};
 
-    for (let i = 0; i < englishLetters.length; i++) {
-        const englishLetter = englishLetters[i];
-        const armenianLetter = armenianLetters[i];
-        charMap[englishLetter] = armenianLetter;
+        for (let i = 0; i < englishLetters.length; i++) {
+            const englishLetter = englishLetters[i];
+            charMap[englishLetter] = armenianLetters[i];
+        }
+        return charMap;
     }
-
-    return charMap;
-}
+});
