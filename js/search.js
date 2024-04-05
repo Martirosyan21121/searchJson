caed9adc1512e375be898ae63e235bce68680786
Narek Martirosyan <narekmartirosyanbm21@gmail.com>
2024-04-05 10:44:41 +0400

    search json

--- js/search.js
+++ js/search.js
@@ -9,14 +9,12 @@ $(document).ready(function () {
 
         $.getJSON('json/data.json', function (data) {
             $.each(data, function (key, value) {
-
                 let armenianSearchInput = convertToArmenian(searchInput.toLowerCase());
-                    console.log(armenianSearchInput, 'armenianSearchInput')
+                console.log(armenianSearchInput, 'armenianSearchInput')
                 if (value.name.search(ignoreCase, unicode) !== -1 ||
                     value.name.toLowerCase().includes(armenianSearchInput.toLowerCase())) {
 
-                    $('#result').append('<li class="list-group-item link-class">' + ' ID -> ' + value.id + ' | Name ->      ' + value.name + '</li>');
-
+                    $('#result').append('<li class="list-group-item link-class">' + ' ID -> ' + value.id + ' | Name -> ' + value.name + '</li>');
                 }
             });
         });
@@ -29,10 +27,14 @@ $(document).ready(function () {
                 if (i < input.length - 1 && generateLetter[input.substr(i, 2)]) {
                     armenianString += generateLetter[input.substr(i, 2)];
                     i += 2;
-                }
-                else {
+                } else {
                     const englishLetter = input[i].toLowerCase();
-                    armenianString += generateLetter[englishLetter] || input[i];
+                    if (englishLetter === 'r') {
+                        armenianString += 'ռ'.toLowerCase()
+                        console.log(armenianString, 'r change to ռ ')
+                    } else {
+                        armenianString += generateLetter[englishLetter] || input[i];
+                    }
                     console.log(armenianString, 'Armenian letter')
                     i++;
                 }
@@ -43,9 +45,15 @@ $(document).ready(function () {
 
     function generateCharMap() {
         const englishLetters = ['a', 'b', 'c', 'd', 'e', 'f',
-            'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'g', 's', 't', 'v', 'x', 'w', 'y', 'z', 'u', 'ch', 'sh', 'ev', 'dz', 'r', 'gh'];
-        const armenianLetters = ['ա', 'բ', 'ց', 'դ', 'ե', 'ֆ', 'գ', 'հ', 'ի', 'ջ',
-            'կ', 'լ', 'մ', 'ն', 'օ', 'պ', 'գ', 'ս', 'տ', 'վ', 'խ', 'ո', 'յ', 'զ', 'ու', 'չ', 'շ', 'և', 'ձ', 'ռր', 'ղ'];
+            'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'g', 's',
+            't', 'v', 'x', 'w', 'y', 'z',
+            'u', 'ch', 'sh', 'ev', 'dz', 'r', 'gh'];
+        const armenianLetters = ['ա', 'բ', 'ց', 'դ', 'ե', 'ֆ',
+            'գ', 'հ', 'ի', 'ջ', 'կ', 'լ',
+            'մ', 'ն', 'օ', 'պ', 'գ', 'ս',
+            'տ', 'վ', 'խ', 'ո', 'յ', 'զ',
+            'ու', 'չ', 'շ', 'և', 'ձ', 'ռր', 'ղ'];
         const charMap = {};
         console.log(charMap, 'chatMap')
 
