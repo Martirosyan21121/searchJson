154bd745284c95da4465b01fe463dd49fffc6dbc
Narek Martirosyan <narekmartirosyanbm21@gmail.com>
2024-04-08 16:01:24 +0400

    search json

--- js/search.js
+++ js/search.js
@@ -14,15 +14,26 @@ $(document).ready(function () {
                 console.log(armenianSearchInput, 'armenianSearchInput')
                 if (value.name.search(ignoreCase, unicode) !== -1) {
                     let $button = $('<button class="link-class">').addClass('list-group-item list-group-item-action').text('ID -> ' + value.id + ' | Name -> ' + value.name);
+
+                    $button.data('id', value.id)
+                    $button.data('name', value.name)
+
                     $('#result').append($button);
                 } else if (value.name.toLowerCase().includes(armenianSearchInput.toLowerCase())) {
                     let $button = $('<button class="link-class">').addClass('list-group-item list-group-item-action').text('ID -> ' + value.id + ' | Name -> ' + value.name);
+
+                    $button.data('id', value.id)
+                    $button.data('name', value.name)
+
                     $('#result').append($button);
                 }
             });
-
             $('.link-class').click(function () {
-                window.location.href = 'data.html';
+                let id = $(this).data('id')
+                let name = $(this).data('name')
+                encodeURIComponent(name)
+                alert(id)
+                window.location.href = 'data.html?id=' + id;
             })
         });
 
@@ -54,13 +65,9 @@ $(document).ready(function () {
                         armenianString += Math.random() > 0.5 ? 'ե' : 'է'.toLowerCase();
                         break
                     case 'c':
-                        if (input[i + 1].toLowerCase() === 'h') {
-                            armenianString += (Math.random() > 0.5) ? 'չ' : 'ճ';
-                            i++;
-                        } else {
-                            armenianString += 'c';
-                        }
-                        break;
+                        armenianString += Math.random() > 0.5 ? 'ց' : 'ծ'.toLowerCase();
+                        break
+
                     default:
                         const englishLetter = input[i].toLowerCase()
                         if (i < input.length - 1 && generateLetter[input.substr(i, 2)]) {
@@ -77,14 +84,14 @@ $(document).ready(function () {
     });
 
     function generateCharMap() {
-        const englishLetters = ['a', 'b', 'c', 'd', 'f',
+        const englishLetters = ['a', 'b', 'd', 'f',
             'g', 'h', 'i', 'k', 'l',
             'm', 'n', 'o', 'g', 's', 'v', 'w', 'y', 'z',
-            'u', 'sh', 'ev', 'dz', 'gh'];
-        const armenianLetters = ['ա', 'բ', 'ց', 'դ', 'ֆ',
+            'u', 'sh', 'ev', 'dz', 'gh', 'ch'];
+        const armenianLetters = ['ա', 'բ', 'դ', 'ֆ',
             'գ', 'հ', 'ի', 'կ', 'լ',
             'մ', 'ն', 'օ', 'գ', 'ս', 'վ', 'ո', 'յ', 'զ',
-            'ու', 'շ', 'և', 'ձ', 'ղ'];
+            'ու', 'շ', 'և', 'ձ', 'ղ', 'չ'];
         const charMap = {};
         for (let i = 0; i < englishLetters.length; i++) {
             const englishLetter = englishLetters[i];
